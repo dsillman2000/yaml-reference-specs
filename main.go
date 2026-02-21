cc3543c1b33d8252dc44fffb787fab221f6a560a
David Sillman <dsillman2000@gmail.com>
2026-02-20 18:15:12 -0800

    fix: implement `main.go` to export a CLI as `yaml-reference-specs`. (#14)

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/cucumber/godog"
+	"github.com/cucumber/godog/colors"
+)
+
+func main() {
+	var cliExecutable string
+	if val, exists := os.LookupEnv("YAML_REFERENCE_CLI_EXECUTABLE"); exists {
+		cliExecutable = val
+	}
+
+	// Parse command line flags
+	format := flag.String("format", "pretty", "Format of output (pretty, junit, etc.)")
+	flag.Parse()
+
+	// Validate CLI executable
+	if cliExecutable == "" {
+		fmt.Fprintf(os.Stderr, "Error: YAML_REFERENCE_CLI_EXECUTABLE environment variable must be set\n")
+		os.Exit(1)
+	}
+
+	if _, err := os.Stat(cliExecutable); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: CLI executable not found at %s: %v\n", cliExecutable, err)
+		os.Exit(1)
+	}
+
+	// Configure godog options
+	var opts = godog.Options{
+		Output: colors.Colored(os.Stdout),
+		Format: *format,
+		Paths:  []string{"features"},
+	}
+
+	// Run the test suite
+	status := godog.TestSuite{
+		Name:                "yaml-reference CLI Test Suite",
+		ScenarioInitializer: InitializeScenario,
+		Options:             &opts,
+	}.Run()
+
+	os.Exit(status)
+}
